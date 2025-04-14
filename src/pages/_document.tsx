256174725bac9b4a1d789b6490d5e6816bac5f8e
Seu Nome <seu.email@exemplo.com>
2025-04-14 15:11:19 -0300

    feat: adiciona Google Tag Manager

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,18 @@ export default function Document() {
   return (
     <Html lang="pt-BR">
       <Head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-K48PTF57');`
+          }}
+        />
+        {/* End Google Tag Manager */}
+
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💜</text></svg>" />
         
         <meta name="description" content="Sistema completo no Notion para psicólogas organizarem prontuários, planilha financeira, agenda, documentos e tudo que precisam para administrar a clínica com leveza ❤️" />
@@ -23,6 +35,17 @@ export default function Document() {
         <meta property="twitter:image" content="https://gestaopluma.sejapluma.com/images/comp.png" />
       </Head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K48PTF57"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <Main />
         <NextScript />
       </body>
